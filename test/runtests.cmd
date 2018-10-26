7511d59cd98d515c73d0d72e6031fb362fa9a0ef
Hao Kung <HaoK@users.noreply.github.com>
2018-10-26 10:53:12 -0700

    Switch path

--- test/runtests.cmd
+++ test/runtests.cmd
@@ -1,3 +1,3 @@
 cd Microsoft.Extensions.Primitives.Tests
-dir C:\Users\VssAdministrator\.dotnet\tools
-C:\Users\VssAdministrator\.dotnet\tools\dotnet test
+dir C:\hostedtoolcache\windows\dncs\2.1.403\x64
+C:\hostedtoolcache\windows\dncs\2.1.403\x64\dotnet.exe test
