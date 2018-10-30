d0208af8de4ba3e8c454a53e414c21aed801ec12
Hao Kung <HaoK@users.noreply.github.com>
2018-10-30 16:32:58 -0700

    More helix support

--- test/runtests.cmd
+++
@@ -1,3 +0,0 @@
-cd Microsoft.Extensions.Primitives.Tests
-dir C:\hostedtoolcache\windows\dncs\2.1.403\x64
-C:\hostedtoolcache\windows\dncs\2.1.403\x64\dotnet.exe test
--- testassets/runtests.cmd
+++
@@ -1,5 +0,0 @@
-rd /s /q scratch
-mkdir scratch
-7za.exe x %1.zip -oscratch -y
-scratch\xunit\tools\net461\xunit.console.exe scratch\%1.dll
-rd /s /q scratch
